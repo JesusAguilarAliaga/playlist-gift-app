ee25ee2ea8951d45e5933318a2e34dfbc6991f31
Jesús Aguilar <aguilaraliagajesus@gmail.com>
2023-12-23 00:44:16 -0400

    funcionalidades del buscado y el filtrado listos

--- src/components/MainList.jsx
+++ src/components/MainList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { axiosMusic } from "../utils/configAxios"
 import MainTrackList from "./MainTrackList"
+import SearchSetting from "./SearchSetting"
 
 /* {
   name: "test",
@@ -13,35 +14,50 @@ import MainTrackList from "./MainTrackList"
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRlMWNlZDFlLWE0ZjctNGE4Yi05MWIyLTFlN2RmOTZhOGIwNiIsImVtYWlsIjoiSmVzdXNAdGVzdC5jb20iLCJuYW1lIjoidGVzdCIsImlhdCI6MTcwMzI3MzYyMH0.VcK7rtAoj0yfydvnGRr1Y2aHimR1LeCFCeYdVfNCric"
 
 
-
 const MainList = () => {
-  const [tracksRecommendations, setTracksRecommendations] = useState([])
+  const [tracksData, setTracksData] = useState([])
+  const [totalTracks, setTotalTracks] = useState("10")
+  const [showSettings, setShowSettings] = useState(false)
   
-  console.log(tracksRecommendations)
+  console.log(tracksData)
   
 
   useEffect(() => {
     //hacer la peticion axios con tu token, ya que este token fue creado en mi base de datos
-
-    /* axiosMusic.get("/api/tracks/recommendations?seed_genres=reggaeton", {
+    axiosMusic.get("/api/tracks/recommendations?seed_genres=reggaeton", {
       headers: {
         Authorization: `JWT ${token}`
-      } 
+      }
     })
-    .then(({ data }) => setTracksRecommendations(data.tracks))
-    .catch(err => console.log(err)) */
+    .then(({ data }) => setTracksData(data.tracks))
+    .catch(err => console.log(err))
   }, [])
+
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    let value = e.target.valueSearch.value
+
+    axiosMusic.get(`/api/tracks?limit=${totalTracks}&q=${value}`, {
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    })
+    .then(({ data }) => setTracksData(data.tracks.items))
+    .catch(err => console.log(err))
+  }
 
   
   return (
     <section className="max-w-[570px] mx-auto py-10 px-16 mt-28 flex flex-col gap-[50px] items-center rounded-lg bg-gradient-to-r from-[rgba(61,46,149,0.35)] to-[#3D2E95] max-sm:mx-3 max-sm:px-4">
-      <form action="" className="w-full h-[55px] px-5 items-center flex justify-between text bg-white/10 rounded-lg">
+      <form onSubmit={onSubmit}className="relative w-full h-[55px] px-5 items-center flex justify-between text bg-white/10 rounded-lg">
         <i className="ri-search-line  text-[20px]"></i>
-        <input className="flex-1 bg-transparent px-4 outline-none font-light" type="text" placeholder="Buscar"/>
-        <i className="ri-list-settings-fill  text-[20px]"></i>
+        <input className="flex-1 bg-transparent px-4 outline-none font-light" name="valueSearch" type="text" placeholder="Buscar"/>
+        <i onClick={() => setShowSettings(!showSettings)} className={`${showSettings ? "ri-close-line" : "ri-list-settings-fill"}  text-[20px]`}></i>
+        {showSettings ? <SearchSetting totalTracks={totalTracks} setTotalTracks={setTotalTracks}/> : null}
       </form>
       <ul className="w-full flex flex-col gap-[20px]">
-        {tracksRecommendations.map((track) => (
+        {tracksData.map((track) => (
           <MainTrackList key={track.id} track={track}/>
         ))}
       </ul>
--- src/components/MainTrackList.jsx
+++ src/components/MainTrackList.jsx
@@ -14,7 +14,7 @@ const MainTrackList = ({track}) => {
 
   return (
     <li className="relative w-full h-[60px] flex gap-5 justify-between items-center hover:bg-[#A284F6]/10 rounded-lg">
-      <img className="size-[60px] rounded-lg" src={track.album.images[2].url} alt="img" />
+      <img className="size-[60px] rounded-lg" src={track.album.images[2]?.url} alt="img" />
       <div className="flex-1">
         <h5 className="text-[15px] font-semibold">{track.album.name}</h5>
         <p className="text-[13px] opacity-40">{formatArtists(track.artists)}</p>
