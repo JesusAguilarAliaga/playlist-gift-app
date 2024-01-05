28a595146a82fb3ccb7b0ce620fb42146fc456f1
Jesús Aguilar <aguilaraliagajesus@gmail.com>
2024-01-05 01:06:21 -0400

    ruta cambiada a browser router

--- src/components/ButtonsCasette.jsx
+++ src/components/ButtonsCasette.jsx
@@ -24,7 +24,7 @@ const ButtonsCasette = ({id}) => {
 
   const onClickShare = () => {
     const urlToShare = window.location.origin
-    const urlComplete = urlToShare + "/#/playlist/public/" + id
+    const urlComplete = urlToShare + "/playlist/public/" + id
 
     navigator.clipboard.writeText(urlComplete)
     .then(() => toastSuccess("link copiado al portapapeles"))
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
           <Provider store={store}>
             <App />
           </Provider>        
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>
 );
